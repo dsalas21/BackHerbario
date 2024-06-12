a8ef8493d5f9b80f129967702901bed589171bc5
dsalas21 <125444146+dsalas21@users.noreply.github.com>
2024-06-11 19:28:19 -0700

    Add files via upload

--- conexion_mysql.js
+++ conexion_mysql.js
@@ -26,6 +26,16 @@ app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 
 
+app.use((req, res, next) => {
+  console.log(`Request URL: ${req.url}, Method: ${req.method}`);
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.send('Backend is running');
+});
+
+
 
 
 router.get('/Usuarios', async function(req, res, next) {
@@ -281,12 +291,12 @@ connection.connect((err) => {
 */
 
 
-/*
+
 
   app.listen(port, () => {
     console.log(`Servidor corriendo en http://localhost:${port}`);
   });
-*/
+
 
 
 module.exports = connection;
